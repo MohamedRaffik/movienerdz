de65809c47097d4d6c5643378baa998778d01a14
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-18 15:46:43 -0500

    semantic works now

--- client/App.js
+++ client/App.js
@@ -1,11 +1,12 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import NavigationBar from './components/containers/NavigationBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">Basic Webpack with React</h1>
+        <NavigationBar />
       </div>
     );
   }
--- client/components/containers/NavigationBar.js
+++ client/components/containers/NavigationBar.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
 
 class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'home'  //
+    };
+  }
   render() {
-    <div>
-      <ul>
-        <li>Home Feed</li>
-        <li>Watch List</li>
-        <li>Favorites</li>
-      </ul>
-    </div>
+
+    const { active } = this.state;
+
+    return (
+      <Menu>
+        <Menu.Item name='home' active={'home' === active}>Home Feed</Menu.Item>
+        <Menu.Item name='watchlater' active={'watchlater' === active}>Watch Later</Menu.Item>
+        <Menu.Item name="fav" active={'fav' === active}>Favorites</Menu.Item>
+      </Menu>
+    );
   }
 }
+
+export default NavigationBar;
