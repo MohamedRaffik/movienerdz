6d773f8befb8432c8ef3dd93ac9cccd18535b4ca
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-18 15:04:28 -0500

    semantic

---
+++ client/components/containers/NavigationBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component {
+  render() {
+    <div>
+      <ul>
+        <li>Home Feed</li>
+        <li>Watch List</li>
+        <li>Favorites</li>
+      </ul>
+    </div>
+  }
+}
