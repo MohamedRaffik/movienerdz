aa4d9e76c47cbb8d520f6c520bbb14bc7b1be70b
kahhauyap <kahhauyap@gmail.com>
2019-01-22 18:36:04 -0500

    Updated modal

--- client/components/presentational/FeedItem.js
+++ client/components/presentational/FeedItem.js
@@ -7,8 +7,9 @@ const FeedItem = (props) => {
 		margin: "1em",
 		boxShadow: "0 4px 8px 0 rgba(255, 255, 255, 0.5), 0 6px 20px 0 rgba(255, 255, 255, 0.3)"
 	}
-	const { overview, release_date, poster_path } = props.data;
+	const { overview, release_date, poster_path, backdrop_path, vote_average } = props.data;
 	let title = props.data.name ? props.data.name : props.data.title;
+
 	return (
 		<Card style={style}>
 			<Image src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
@@ -17,7 +18,7 @@ const FeedItem = (props) => {
 				<Card.Meta>
 					<span className="movie">{release_date}</span>
 				</Card.Meta>
-				<MovieModal/>
+				<MovieModal title={title} overview={overview} release_date={release_date} backdrop_path={backdrop_path} vote_average={vote_average}/>
 			</Card.Content>
 		</Card>
 	);
--- client/components/presentational/MovieModal.js
+++ client/components/presentational/MovieModal.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 
-const MovieModal = () => (
-  <Modal trigger={<Button style={{backgroundColor: "white"}}>Info</Button>}>
-    <Modal.Header>Select a Photo</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
-      <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>We've found the following gravatar image associated with your e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
-      </Modal.Description>
-    </Modal.Content>
-  </Modal>
-)
+const MovieModal = (props) => {
+    console.log(props);
+    const {title, overview, vote_average, release_date, backdrop_path} = props;
+    
+    const style = {
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto"
+    }
+
+    const modalStyle = {
+            backgroundColor: "black",
+            margin: "1em",
+            boxShadow: "0 4px 100px 0 rgba(255, 255, 255, 0.5), 0 6px 20px 0 rgba(255, 255, 255, 0.3)"
+        }
+    
+    const infoStyle = {
+        backgroundColor:"black", 
+        color: "white", 
+        textAlign: "center",
+        borderBottom: "1px solid white",
+        paddingBottom: "8px"
+    }
+    
+    return (
+        <Modal size="small" style={modalStyle} trigger={<Button style={{backgroundColor: "white"}}>Info</Button>}>
+            <Modal.Header style={{backgroundColor:"black", color:"white", textAlign:"center"}}>{title}</Modal.Header>
+            <Image  size="massive" fluid="true" src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`} />
+            <Header style={infoStyle}>Rating: {vote_average} Release: {release_date}</Header>
+        
+            <Modal.Description>
+                <p style={{color:"white", textAlign:"center", fontSize: "20px", margin: "1em"}}>{overview}</p>
+            </Modal.Description>
+
+        </Modal>
+    );
+}
 
 export default MovieModal;
