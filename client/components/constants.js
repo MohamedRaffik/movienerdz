c495b668532821d0f28e8e6f0dee48bd23848d9c
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-22 13:48:33 -0500

    Filter works new size for bar

--- client/components/constants.js
+++ client/components/constants.js
@@ -1,9 +1,11 @@
+import { FILTER_ACTIONS } from '../actions';
+const { TRENDING, UPCOMING, POPULAR, TOP_RATED, PLAYING_NOW, SEARCH, WATCH_LATER, FAVORITES } = FILTER_ACTIONS;
+
 export const FILTER_OPTIONS = [
-  'Show Trending Movies',
-  'Show Upcoming Movies',
-  'Show Popular Movies',
-  'Show Top Rated Movies',
-  'Show Playing Now Movies'
+  [ 'Show Trending Movies', TRENDING ],
+  [ 'Show Upcoming Movies', UPCOMING ],
+  [ 'Show Popular Movies', POPULAR ],
+  [ 'Show Top Rated Movies', TOP_RATED ]
 ];
 
 export const GENRE_OPTIONS = [
