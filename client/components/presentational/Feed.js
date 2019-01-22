7d4028c4009b8404f63ba3187d4fbdf080ff84e4
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-22 00:06:17 -0500

    updated feed

--- client/components/presentational/Feed.js
+++ client/components/presentational/Feed.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import FeedItem from './FeedItem';
+import { Grid } from 'semantic-ui-react';
 import { FILTER_ACTIONS } from '../../actions';
-import { queryResults } from '../constants';
 const { TRENDING, UPCOMING, POPULAR, TOP_RATED, PLAYING_NOW, SEARCH, WATCH_LATER, FAVORITES } = FILTER_ACTIONS;
 
 
 const Feed = (props) => {
-	const feedStyling = {
-		"display": "inline-block",
-		"margin": "20px"
-	}
-
 	let results = [];
 	const filter = props.filter;
 	if (filter === TRENDING) results = props.trending;
@@ -19,15 +14,25 @@ const Feed = (props) => {
 	else if (filter === TOP_RATED) results = props.top_rated;
 	else if (filter === PLAYING_NOW) results = props.playing_now;
 	
-	const items = results.map((row, index) =>
-		<FeedItem key={index} data={row}/>
-	);
+	const items = [];
+	for (let i = 0; i < results.length; i+=4) {
+		items.push(
+			<Grid.Row key={i} centered={true}>
+				<FeedItem data={results[i]} />
+				<FeedItem data={results[i+1]} />
+				<FeedItem data={results[i+2]} />
+				<FeedItem data={results[i+3]} />
+			</Grid.Row>
+		);
+	} 
 
 	return (
-		<div className="feed" style={feedStyling}>
-			<h2>results:</h2>
-			{items}	
-		</div>
+		<Grid relaxed={true} padded={true}>
+			<Grid.Row style={{"margin-left": "2em"}}>
+				<h2>Results:</h2>
+			</Grid.Row>
+			{items}
+		</Grid>
 	);
 }
 
