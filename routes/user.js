7e11bff1a88006c76f5623d5d980b88dd5f9a6e0
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-23 16:31:40 -0500

    fixed user backend

--- routes/user.js
+++ routes/user.js
@@ -1,55 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+module.exports = (passport) => {
+	router.post('/signup', (req, res, next) => {
+		passport.authenticate('local-signup', (err, user, info) => {
+			if (err) return next(err);
+			if (!user) return next({ error: true, message: info });
+			req.login(user, (loginErr) => {
+				if (loginErr) return next(loginErr);
+				return res.json(user);
+			})
+		})(req, res, next);
+	});
 
-
-function signup(passport){
-
-	router.post('/signup',function(req,res,next){
-		passport.authenticate('local-signup', function(err,user,info){
-			if(err){
-				return next(err);
-			}
-			if(!user){
-				return next({error: true, message:info});
-			}
-
-			req.login(user,function(loginErr){
-				if(loginErr){
-					return next(loginErr);
-				}
+	router.post('/login', function (req, res, next) {
+		passport.authenticate('local-login', function (err, user, info) {
+			if (err) return next(err);
+			if (!user) return next({ error: true, message: info });
+			req.login(user, function (loginErr) {
+				if (loginErr) return next(loginErr);
 				return res.json(user);
 			});
-		})(req,res,next);
-
+		})(req, res, next);
 	});
+
 	return router;
-};
-
-function login(passport){
-	router.post('/login', function(req,res,next){
-		passport.authenticate('local-login', function(err,user,info){
-			if(err){
-				return next(err);
-			}
-			if(!user){
-				return next({error: true, message:info});
-			}
-
-			req.login(user,function(loginErr){
-				if(loginErr){
-					return next(loginErr);
-				}
-				return res.json(user);
-			});
-		})(req,res,next);
-
-	});
-	return router;
-};
-
-
-module.exports = {
-	signup, login
-
 }
