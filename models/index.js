805b756e4266975c2a8afa3247beca8f2bb1c3a2
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-24 01:01:00 -0500

    -

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const UserModel = require('./User');
 require('dotenv').config();
-const db = new Sequelize("postgres://postgres:```@localhost:5432/postgres");
+const DATABASE_URL = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : 'postgres://postgres:```@localhost:5432/postgres';
+const db = new Sequelize(DATABASE_URL);
 
 const User = UserModel(db, Sequelize);
 
